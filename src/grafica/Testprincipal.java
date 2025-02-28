2e2197d4a7d2240bf61c042f8421ba8f60b7a866
Nisoca <nico29.com@gmail.com>
2025-02-27 21:47:33 -0300

    Correcciones en Testprincipal.java

--- src/grafica/Testprincipal.java
+++ src/grafica/Testprincipal.java
@@ -23,10 +23,13 @@ public class Testprincipal {
 		p1.ventaBoleto(b2);
 		float resu = p1.montoRecaudado();
 		System.out.println("El Monto es:" + " " + resu);
+		
 		List<VOboletolistado> vobl = p1.listarBoletos();
 		vobl.forEach((vo)-> System.out.println("El boleto es: " + vo.getNumero()+ ", " + vo.getCelular() + ", " + vo.getEdad() + ", " + vo.getNombrepasajero() + ", " + vo.getMonto()));
-		List<VOminivanlistado>  vom = m.listarMinivan();
-		System.out.println("La minivan es: " + vom.getMatricula()+ ", " + vom.getMarca() + ", " + vom.getModelo() + ", " + vom.getCantasientos() + ", " + vom.getCantpaseosasignados());
+		
+		List<VOminivanlistado> vom = m.listarMinivan();
+		vom.forEach((vo)-> System.out.println("La minivan es: " + vo.getMatricula()+ ", " + vo.getMarca() + ", " + vo.getModelo() + ", " + vo.getCantasientos() + ", " + vo.getCantpaseosasignados()));
+		
 		List<VOpaseolistado> vopl = p.listadoPaseos();
 		vopl.forEach((vo)-> System.out.println("El paseo es: " + vo.getCodigo() + ", " + vo.getDestino() + ", " + vo.getMaxboletos() + ", " + vo.getMonto() + ", " + vo.getPrecio()));
 	}
