dc55c3e00ca667bc86a273d787da7cee4fd935f4
MateMole <mjmendiondo@gmail.com>
2025-02-28 19:53:38 -0300

    cambios de cabezales en la interfaz

--- src/logica/fachada/IFachada.java
+++ src/logica/fachada/IFachada.java
@@ -1,20 +1,28 @@
 package logica.fachada;
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 import java.time.LocalTime;
+import java.util.ArrayList;
+
+import logica.colecciones.Boletos;
+import logica.excepciones.LogicaException;
+import logica.excepciones.PersistenciaException;
 import logica.valueobject.VOboletoingreso;
 import logica.valueobject.VOminivan;
 import logica.valueobject.VOpaseoingreso;
+import logica.valueobject.VOpaseolistado;
 public interface IFachada extends Remote {
 	///Metodos
-	public void VentaBol(int codigo,VOboletoingreso bo);
-	public void RegisMin(VOminivan mini);
-	public VOminivan LisMin();
-	public void RegisPas(int cod,String des,LocalTime HP,LocalTime HL,float Prec,int MaxBol);
-	public VOpaseoingreso LisPasAsMin();
-	public VOpaseoingreso LisPasDes(String des);
-	public VOpaseoingreso LisDisBol(int maxb);
-	public VOpaseoingreso LisPasBolVen(String cod);
+	
+	public void RegisMin(VOminivan mini) throws RemoteException,LogicaException;
+	public void VentaBol(String codigo,Boletos bo) throws RemoteException,LogicaException;
+	public void recuperardatos() throws RemoteException, PersistenciaException;
+	public void RegisPas(String cod,String des,LocalTime HP,LocalTime HL,float Prec,int MaxBol) throws RemoteException,LogicaException;
+	public ArrayList<VOpaseolistado>  LisPasAsMin(String mat);
+	public ArrayList<VOpaseolistado> LisPasDes(String des);
+	public VOpaseoingreso LisDisBol(int maxb,Boletos bo) throws RemoteException,LogicaException;
+	public  ArrayList<VOpaseolistado> LisPasBolVen(String cod) throws RemoteException,LogicaException;
 	public float MonRec(String cod);
-	public void respaldardatos();	
-	public void recuperardatos();
+	public void respaldardatos() throws RemoteException, PersistenciaException;
+	public void recuperardatos() throws RemoteException, PersistenciaException;
 }
